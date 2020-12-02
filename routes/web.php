d4b71523f0d77d89a8933155745bcec852498619
eugenie ashuza <eugenieashuza@gmail.com>
2020-12-02 10:24:55 +0200

    Set register, reset and confirm routes to false

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false, 'reset' => false, 'confirm' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
