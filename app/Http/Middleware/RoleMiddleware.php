a6f8cbd2227bfe67b0a66002db8af22467820df2
Kezimana Aime Angelo <kezangelo@gmail.com>
2020-12-02 13:23:31 +0200

    Add RoleMiddleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, String $role)
+    {
+        $user = Auth::user();
+        if ($user->role->name == $role)
+            return $next($request);
+
+        return redirect("/{$user->role->name}_dashboard");
+    }
+}
