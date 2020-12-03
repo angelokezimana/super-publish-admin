969415db41c531396be337830c9d2a339d6b7954
Kezimana Aime Angelo <kezangelo@gmail.com>
2020-12-03 15:06:27 +0200

    Register users routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('templates.default');
-});
+// Login Page
+Route::get('/', 'Auth\LoginController@showLoginForm');
 
 Auth::routes(['register' => false, 'reset' => false, 'confirm' => false]);
 
@@ -23,4 +22,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('role:admin')->group(function () {
     Route::get('/users', 'UserController@index')->name('users.index');
+    Route::post('/users', 'UserController@store')->name('users.store');
+    Route::put('/users/{user}', 'UserController@update')->name('users.update');
+    Route::put('users/{user}/suspend', 'UserController@suspend')->name('users.suspend');
+    Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
 });
