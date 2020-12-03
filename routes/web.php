90533ce29d5dc8b858c267f351061af1a9ad1f5c
Eugenie Ashuza <eugenieashuza@gmail.com>
2020-12-03 10:58:30 +0100

    Add root of categories:index,create,store

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin_dashboard', 'Admin\DashboardController@index')->middleware('role:admin');
 Route::get('/editor_dashboard', 'Editor\DashboardController@index')->middleware('role:editor');
+
+//CRUD for categories
+Route::get('categories', 'CategoriesController@index');
+Route::get('categories/create', 'CategoriesController@create');
+Route::post('categories', 'CategoriesController@store');
