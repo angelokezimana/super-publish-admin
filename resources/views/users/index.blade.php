b943a40f91635052fab786198bcf233c384d452c
Kezimana Aime Angelo <kezangelo@gmail.com>
2020-12-04 14:47:17 +0200

    Edit users table

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -25,11 +25,12 @@
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->username }}</td>
                 <td>{{ $user->role->name }}</td>
-                <td>{{ $user->banned_at }}</td>
+                <td>{!! $user->banned_at ?? '<label class="badge badge-info">Pas bloqu&eacute;</label>' !!}</td>
                 <td>
-                    <a href="{{ route('users.edit', $user) }}" class="btn btn-primary btn-sm" data-toggle="tooltip" data-placement="top" title="Modifier cet utilisateur">
-                        <i class="fas fa-edit"></i>
-                    </a>
+
+                    <button type="button" class="btn btn-primary btn-sm text-white btn-edit-user" data-toggle="modal" data-target="#crud-modal-user" data-id="{{ $user->id }}" data-last_name="{{ $user->last_name }}" data-first_name="{{ $user->first_name }}" data-email="{{ $user->email }}" data-username="{{ $user->username }}" data-role_id="{{ $user->role->id }}" data-role_name="{{ $user->role->name }}">
+                        <i class="fa fa-edit"></i>
+                    </button>
 
                     <form action="{{ route('users.suspend', $user) }}" method="post" class="inline">
                         @csrf
@@ -37,11 +38,11 @@
 
                         @if ($user->banned_at)
                         <button type="submit" class="btn btn-info btn-sm" onclick="return confirm('Voulez-vous vraiment débloquer cet utilisateur ?')" data-toggle="tooltip" data-placement="top" title="Débloquer cet utilisateur">
-                            <i class="fas fa-unlock-alt"></i>
+                            <i class="fa fa-unlock-alt"></i>
                         </button>
                         @else
                         <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Voulez-vous vraiment bloquer cet utilisateur ?')" data-toggle="tooltip" data-placement="top" title="Bloquer cet utilisateur">
-                            <i class="fas fa-user-lock"></i>
+                            <i class="fa fa-lock"></i>
                         </button>
                         @endif
                     </form>
@@ -50,7 +51,7 @@
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Voulez-vous vraiment supprimer cette entrée ?')" data-toggle="tooltip" data-placement="top" title="Supprimer cet utilisateur">
-                            <i class="fas fa-trash-alt"></i>
+                            <i class="fa fa-trash"></i>
                         </button>
                     </form>
                 </td>
@@ -59,4 +60,10 @@
         </tbody>
     </table>
 </div>
+
+@include('users.partials.crud-modal-user')
+@endsection
+
+@section('scripts')
+    <script src="{{asset('js/User.js')}}"></script>
 @endsection
