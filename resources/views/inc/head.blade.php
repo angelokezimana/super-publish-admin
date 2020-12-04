2280366738739a825b585c5129dfd7e1c1011ba7
Eugenie ashuza <eugenieashuza@gmail.com>
2020-12-04 14:47:43 +0100

    Add tag  meta name csrf-token

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{ config('app.name') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
