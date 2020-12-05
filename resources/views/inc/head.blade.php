16bf3c9db8025d47abada99d765236772e6571b5
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-05 21:08:32 +0200

    Add meta name csrf-token

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
     <meta name="description" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
 		============================================ -->
