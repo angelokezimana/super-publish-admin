27fa26e0e831649a4852314dc6a536d3585c771d
Kezimana Aime Angelo <kezangelo@gmail.com>
2020-12-07 10:17:02 +0200

    Add index method

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Publication;
 use Illuminate\Http\Request;
 
 class PublicationController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $publications_without_paginate = Publication::with('category')->where('actif', 1);
+        $publications = Publication::with('category')->where('actif', 1)->paginate(9);
+
+        return view('publications.index', [
+            'publications_without_paginate' => $publications_without_paginate,
+            'publications' => $publications
+        ]);
+    }
 }
