3d762e92ad720f7cba7ab2782de19ecb378ea64a
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-08 22:42:59 +0200

    Add class 'selected' when class 'custom-file-input' is changed

--- public/js/Publication.js
+++ public/js/Publication.js
@@ -1,34 +1,9 @@
 (function ($) {
     "use strict";
 
-    $(document).on('click', "#id-form", function () {
-        var content = $('#summernote').summernote('code');
-        var title = $('#title').val();
-        var category_id = $('#category_id').val();
-
-        $.ajax({
-            type: 'POST',
-            url: '/publications',
-            data: {
-                content: content,
-                title: title,
-                category_id: category_id
-            },
-            success: function (data) {
-                if (data.errors) {
-                    if(data.errors.content) {
-                        
-                    }
-                }
-                else {
-                    window.location.replace('/publications');
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert(errorThrown);
-            },
-            dataType: 'json'
-        });
+    $(".custom-file-input").on("change", function () {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     });
 
 })(jQuery); 
