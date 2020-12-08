cb4a11d60f65da6d8c995c6acb40bdbfb9f1270d
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-08 22:44:36 +0200

    Add ckeditor script

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -6,6 +6,7 @@
     <meta name="description" content="{{ config('app.name') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="{{asset('js/ckEditor/ckeditor.js')}}"></script>
     <!-- favicon
 		============================================ -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
