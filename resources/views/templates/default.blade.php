a0591fca00cfb5b315c9f8ab5beb2e3a3fcdde0c
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-09 21:33:22 +0200

    Change the default template

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,33 +1,69 @@
-<!doctype html>
-<html class="no-js" lang="en">
-
+<!DOCTYPE html>
+<html lang="fr">
 @include('inc.head')
 
-<body>
-    <!--[if lt IE 8]>
-		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-    <!-- Start Left menu area -->
-    @include('inc.sidebar')
-    <!-- End Left menu area -->
-    
-    <!-- Start Welcome area -->
-    <div class="all-content-wrapper">
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-        @include('inc.nav')
+        @include('inc.sidebar')
 
-            <div class="container-fluid mt-custom">
-                <div class="row">
-                    @yield('content')
-                </div>              
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('inc.topbar')
+
+                @yield('content')
+
+            </div>
+
+            @include('inc.footer')
+
+        </div>
+    </div>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Prêt à partir ?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Sélectionnez "Déconnexion" ci-dessous si vous êtes prêt à mettre fin à votre
+                    session en cours.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+                    <a class="btn btn-primary" href="#"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Déconnexion</a>
+
+                    <form id="logout-form" action="#" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+
+                </div>
             </div>
         </div>
-      
+    </div>
 
-        @include('inc.footer')
-        
-    @include('inc.script')
-   
+    <!-- Scripts -->
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('js/dataTables.min.js')}}"></script>
+    <script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('js/chosen.jquery.min.js') }}"></script>
 </body>
 
 </html>
