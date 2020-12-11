3906283dbde097f3c59ca52daa5730215327d4f4
Kezimana Aime Angelo <kezangelo@gmail.com>
2020-12-11 12:13:32 +0100

    Update Categorie:add function categories

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
     //
+    public function categories()
+    {
+        # code...
+        return $this->hasMany(Categorie::class,'category_id');
+    }
 }
