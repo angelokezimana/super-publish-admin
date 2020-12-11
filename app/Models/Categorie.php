e7daefe99f9078330ecfb7b018d005dd938189d1
Kezimana Aimé Angelo <kezangelo@gmail.com>
2020-12-11 13:30:15 +0200

    Add categories relationship

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
+    public function categories()
+    {
+        return $this->hasMany('App\Models\Categorie', 'category_id');
+    }
+
     public function publications()
     {
         return $this->hasMany('App\Models\Publication', 'category_id');
